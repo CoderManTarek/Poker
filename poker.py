0c3385c8a5743c6924f7b90ee3d2f9be4b79f6e1
Stephen Zhang <43100355+zstephenj@users.noreply.github.com>
2022-02-26 16:43:00 -0600

    Merged in tkinter code for window, table, chatbox

--- poker.py
+++ poker.py
@@ -1,9 +1,7 @@
 import random
+from operator import attrgetter
 from tkinter import *
 import tkinter as tk
-
-#done
-from operator import attrgetter
 
 class Card:
   #constructor
@@ -359,18 +357,23 @@ def main():
 
   #test shuffle
   table.deal()
+  
+  gui = Tk()
+  gui.title("Poker App")
+  gui.config(bg="#D3D3D3")
+  gui.geometry("1200x800")
+  gui.resizable(False, False)
+  #widgets
+  #room frame
+  room_frame = Frame(gui, bg="#505b62", width=1200, height=600)
+  room_frame.pack(padx=5, pady=5)
+  #table frame
+  table_frame = Frame(room_frame, bg="#35654d", width=800, height=300)
+  table_frame.place(x=200,y=150)
+  # #35654d poker green
 
-  # gui = Tk()
-  # #widgets
-  # gui.title("Poker App")
-  # gui.geometry("800x800")
-  # gui.mainloop()
+
+  gui.mainloop()
 
 
 main()
-
-# gui = Tk()
-# #widgets
-# gui.title("Poker App")
-# gui.geometry("800x800")
-# gui.mainloop()
